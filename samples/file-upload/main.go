800419599010badfda54fc6e8957e55bef4ac63e
Syed Osama Anwer <osama.anwer2@securiti.ai>
2025-11-17 15:40:57 +0500

    Revert "Revert "Update module reference""
    This reverts commit 881a0bde0caeb35c2edb6ce40dbb6de321144ad2.

--- samples/file-upload/main.go
+++ samples/file-upload/main.go
@@ -11,9 +11,9 @@ import (
 	"net/url"
 	"os"
 
-	"github.com/infracloudio/msbotbuilder-go/core"
-	"github.com/infracloudio/msbotbuilder-go/core/activity"
-	"github.com/infracloudio/msbotbuilder-go/schema"
+	"github.com/securitiai/msbotbuilder-go/core"
+	"github.com/securitiai/msbotbuilder-go/core/activity"
+	"github.com/securitiai/msbotbuilder-go/schema"
 )
 
 func putRequest(u string, data []byte) error {
